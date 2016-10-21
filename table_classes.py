99b497562dd661f165f79881a9053d4ab591242d
csmeixell <csmeixell@gatech.edu>
2016-10-21 11:48:33 -0400

    Add files via upload

---
+++ table_classes.py
@@ -0,0 +1,54 @@
+class ID_Table:
+    table_name = "GEO_ID"
+    var1 = "Id"#"GEO.id"
+    type1 = "string"
+    var2 = "Id2"#"GEO.id2"
+    type2 = "string"
+    var3 = "Geography"#"GEO.display-label"
+    type3 = "string"
+
+class Median_Age_Sex_Table:
+    table_name = "Age Brackets"
+    Id = "Id"
+    Id_type = "string"
+    data_types_all_int = True
+    var1 = "Total"
+    var2 = "Under 5 years"
+    var3 = "5 to 9 years"
+    var4 = "10 to 14 years"
+    var5 = "15 to 17 years"
+    var6 = "18 and 19 years"
+    var7 = "20 years"
+    var8 = "21 years"
+    var9 = "22 to 24 years"
+    var10 = "25 to 29 years"
+    var11 = "30 to 34 years"
+    var12 = "35 to 39 years"
+    var13 = "40 to 44 years"
+    var14 = "45 to 49 years"
+    var15 = "50 to 54 years"
+    var16 = "55 to 59 years"
+    var17 = "60 and 61 years"
+    var18 = "62 to 64 years"
+    var19 = "65 and 66 years"
+    var20 = "67 to 69 years"
+    var21 = "70 to 74 years"
+    var22 = "75 to 79 years"
+    var23 = "80 to 84 years"
+    var24 = "85 years and over"
+    var25 = "Total"
+    var26 = "Under 5"
+    var27 = "5 to 17"
+    var28 = "18 to 24 years"
+    var29 = "25 to 39 years"
+    var30 = "40 to 64 years"
+    var31 = "65 years and over"
+    var32 = "75 years and over"
+    var33 = "85 years and over"
+
+class Race_Table:
+
+    
+    
+    
+    
