1e009758e0ec2035b6ab4f91153464cafba34bf2
Pratik Shah <hello@pratik-shah.me>
2016-10-25 11:59:27 -0400

    [CHG] - Changed the category comparison to == rather than is

--- tables/__init__.py
+++ tables/__init__.py
@@ -11,10 +11,10 @@ def updateIdTableWithNewCSVFile(csvFile) :
 
 def parseAndInsertData(category, csv_file, table_name, from_Date, to_Date) :
 	print(category)
-	if category is DataCategories.DEMOGRAPHICS :
+	if category.lower() == DataCategories.DEMOGRAPHICS.lower() :
 		print("Inside Demographics")
 		parseAndInsertDemographicData(csv_file, table_name, from_Date, to_Date)
 
-	if category is DataCategories.CALCULATIONS :
+	if category.lower() == DataCategories.CALCULATIONS.lower() :
 		print("Inside calculations")
 		parseAndInsertCalculationsData(csv_file, table_name, from_Date, to_Date)
