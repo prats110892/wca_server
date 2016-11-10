90bc5c7f9e364e2ae8335fe5d3eda80245706bbe
csmeixell <csmeixell@gatech.edu>
2016-11-09 20:19:19 -0500

    Update __init__.py

--- tables/__init__.py
+++ tables/__init__.py
@@ -14,6 +14,24 @@ def parseAndInsertData(category, csv_file, table_name, from_Date, to_Date) :
 	if category.lower() == DataCategories.DEMOGRAPHICS.lower() :
 		print("Inside Demographics")
 		parseAndInsertDemographicData(csv_file, table_name, from_Date, to_Date)
+	elif category.lower() == DataCategories.EDUCATION.lower() :
+		print("Inside Education")
+		parseAndInsertDemographicData(csv_file, table_name, from_Date, to_Date)
+	elif category.lower() == DataCategories.EMPLOYMENT.lower() :
+		print("Inside Employment")
+		parseAndInsertDemographicData(csv_file, table_name, from_Date, to_Date)
+	elif category.lower() == DataCategories.HEALTH.lower() :
+		print("Inside Health")
+		parseAndInsertDemographicData(csv_file, table_name, from_Date, to_Date)
+	elif category.lower() == DataCategories.HOUSING.lower() :
+		print("Inside Housing")
+		parseAndInsertDemographicData(csv_file, table_name, from_Date, to_Date)
+	elif category.lower() == DataCategories.INCOME.lower() :
+		print("Inside Income")
+		parseAndInsertDemographicData(csv_file, table_name, from_Date, to_Date)
+	elif category.lower() == DataCategories.TRANSPORTATION.lower() :
+		print("Inside Transportation")
+		parseAndInsertDemographicData(csv_file, table_name, from_Date, to_Date)
 
 def parseAndInsertCalculations(csv_file, table_name, from_Date) :
 	print("Inside calculations")
