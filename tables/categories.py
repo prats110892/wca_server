3c500a449ef258f6385a94944d5cf641fbefdc63
csmeixell <csmeixell@gatech.edu>
2016-11-09 20:13:44 -0500

    Update categories.py

--- tables/categories.py
+++ tables/categories.py
@@ -1,4 +1,10 @@
 
 class DataCategories(object) :
 	DEMOGRAPHICS = "demographics"
+	EDUCATION = "education"
+	EMPLOYMENT = "employment"
+	HEALTH = "health"
+	HOUSING = "housing"
+	INCOME = "income"
+	TRANSPORTATION = "transportation"
 	CALCULATIONS = "calulations"
