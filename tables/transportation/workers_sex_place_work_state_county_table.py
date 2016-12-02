00f87017736cfc06ef807464b2700360a4ecc9ad
JD Arthur <jdarthur@mail.com>
2016-12-02 15:54:43 -0500

    changed INSERT to REPLACE for overwrite

--- tables/transportation/workers_sex_place_work_state_county_table.py
+++ tables/transportation/workers_sex_place_work_state_county_table.py
@@ -12,7 +12,7 @@ class WORKERS_SEX_PLACE_WORK_STATE_COUNTY_Table(Base_Table):
 
 	def getInsertQueryForCSV(self, csvFile, fromYear, toYear) :
 		skipCount = 0
-		insertDataQuery = """INSERT INTO `{0}` VALUES """.format(self.table_name)
+		insertDataQuery = """REPLACE INTO `{0}` VALUES """.format(self.table_name)
 		for line in csvFile:
 			row = line.split(",")
 			if (skipCount < Base_Table.num_of_rows_to_leave) :
