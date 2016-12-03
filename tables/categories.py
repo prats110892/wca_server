dc5a84df8c865853230c10cdf26a008cda3a0f46
JD Arthur <jdarthur@mail.com>
2016-12-02 21:01:51 -0500

    added some documentation for categories.py

--- tables/categories.py
+++ tables/categories.py
@@ -1,3 +1,6 @@
+#This file contains the names of all the types of data (which each contain a lot of sub-types)
+#To add new classes of data, this file will need to be updated, as well as the __init__.py in each folder
+
 
 class DataCategories(object) :
 	ID = "GEO_ID"
